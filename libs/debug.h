1cd3207b538d72023f804ce8e8b5ef1e801c7317
Doo <doo11gms@gmail.com>
2018-12-01 23:42:00 +0900

    update debug.h

--- libs/debug.h
+++ libs/debug.h
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 void LogNewline();
+void DebugLogNewline();
 void LogSeparator();
 void LogBoldSeparator();
 void LogLabel(char*);
 void LogInt(int);
 void LogIntArray(int*, int);
+void LogChar(char);
+void LogCharArray(char*, int);
 
 void LogNewline() {
   printf("\n");
+}
+
+void DebugLogNewline() {
+  #ifdef DEBUG
+  LogNewline();
+  #endif
 }
 
 void LogSeparator() {
@@ -31,6 +40,17 @@ void LogIntArray(int* target, int length) {
   for(int i = 0; i < length; i++) {
     if (i != 0) printf(" ");
     printf("%d", target[i]);
+  }
+  printf("\n");
+}
+
+void LogChar(char target) {
+  printf("%c\n", target);
+}
+
+void LogCharArray(char* target, int length) {
+  for(int i = 0; i < length; i++) {
+    printf("%c", target[i]);
   }
   printf("\n");
 }
